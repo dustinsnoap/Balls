a01cb327624b89a4aa21ca88f0e1134a3ffbc5b1
Dustin Snoap <afrobacon@gmail.com>
2018-12-19 21:53:30 -0500

    learned about and implemented classes

--- js/balls.js
+++ js/balls.js
@@ -1,21 +1,50 @@
 window.onload = function () {
-    run();
+    //set up listeners
+    let canvas = new Canvas("sack")
+    run(canvas);
 }
 
-function run() {
-    window.setInterval(clock,30);
+function startListeners() {
+    let ballDropper = document.getElementById("btn_drop");
+    ballDropper.addEventListener("click", /*make ball*/);
 }
-function clock() {
-    let c = getCanvas();
+
+function run(canvas) {
+    window.setInterval(clock(canvas),30);
+}
+function clock(canvas) {
+    let balls = [];
+    let mouse = {x: 0, y: 0};
     //clear canvas
-    c.ctx.clearRect(0,0,c.width,c.height);
+    canvas.clear();
     //draw balls
     console.log("tick")
 }
-function getCanvas() {
-    let canvas = document.getElementById('sack');
-    let ctx = canvas.getContext('2d');
-    let height = window.innerHeight - 50;
-    let width = window.innerWidth;
-    return {canvas, ctx, height, width};
+
+//\/==CLASSES==\/
+class Canvas {
+    constructor(name) {
+        this.name = name;
+        this.canvas = document.getElementById(this.name);
+        this.ctx = this.canvas.getContext("2d");
+        this.height = window.innerHeight - 50;
+        this.width = window.innerWidth;
+    }
+    clear() {
+        this.ctx.clearRect(0,0,this.width,this.height);
+    }
+}
+class Ball {
+    constructor(x, y, dx, dy, color, radius) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.color = color;
+        this.radius = radius;
+    }
+    changeColor() {}
+    changeSpeed() {}
+    move() {}
+    kill() {}
 }
