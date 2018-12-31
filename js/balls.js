d5298f268e6be0e3a2895ce6c12ee5a671fb4c6e
dustinsnoap <afrobacon@gmail.com>
2018-12-31 01:29:18 -0500

    updated ball.move balls now move! balls bounce off walls too.

--- js/balls.js
+++ js/balls.js
@@ -23,7 +23,7 @@ function setupCanvas() {
     canvas.width = window.innerWidth;
     canvas.balls = [];
     canvas.addBall = function() {
-        canvas.balls.push(new Ball(getRand(1000), getRand(1000), 1, "red", 10));
+        canvas.balls.push(new Ball(getRand(canvas.width), getRand(canvas.height), 1, 1, "red", 10));
     };
     canvas.clear = function() {
         console.log("tes");
@@ -32,7 +32,7 @@ function setupCanvas() {
 }
 
 function run() {
-    window.setInterval(clock,500);
+    window.setInterval(clock,1);
 }
 function getRand(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -45,8 +45,9 @@ function clock() {
     // let balls = [];
     // let mouse = {x: 0, y: 0};
     //draw balls
+    canvas.clear();
     canvas.balls.forEach(function(ball) {
-        // ball.move();
+        ball.move();
         ball.draw();
     });
     console.log(canvas.balls);
@@ -54,10 +55,11 @@ function clock() {
 
 //\/==CLASSES==\/
 class Ball {
-    constructor(x, y, speed, color, radius) {
+    constructor(x, y, dx, dy, color, radius) {
         this.x = x;
         this.y = y;
-        this.speed = speed;
+        this.dx = dx;
+        this.dy = dy;
         this.color = color;
         this.radius = radius;
         this.canvas = getCanvas();
@@ -76,8 +78,10 @@ class Ball {
         this.speed = speed;
     }
     move() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += this.dx;
+        this.y += this.dy;
+        if(this.x <= 0 || this.x >= this.canvas.width) this.dx *= -1;
+        if(this.y <= 0 || this.y >= this.canvas.height) this.dy *= -1;
     }
     kill() {}
 }
